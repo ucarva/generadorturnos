bf9f8d5d4a2c78e92e1983bb9f01079c8dedabd4
Uriel Carvajalino <uriel_carva@hotmail.com>
2024-09-03 11:20:12 -0500

    mejoras al generador

--- server.js
+++ server.js
@@ -1,36 +1,49 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+// Middleware para poder leer `req.body`
 app.use(express.json());
-app.use(express.static('public')); // Servir archivos estáticos
+app.use(express.urlencoded({ extended: true }));
 
-// Variable para mantener el estado del último número de turno generado
-let ultimoNumeroTurno = 0;
+app.use(express.static('public')); // Asegúrate de que 'public' es el directorio donde guardas tus archivos estáticos
+
+app.get('/turno', (req, res) => {
+  const { mensaje, tipo } = req.query;
+  res.redirect(`/turno.html?mensaje=${encodeURIComponent(mensaje)}&tipo=${encodeURIComponent(tipo)}`);
+});
+
+// Variables para mantener el estado del último número de turno generado para cada tipo
+let ultimoNumeroCredito = 0;
+let ultimoNumeroAsesoria = 0;
+let ultimoNumeroCaja = 0;
 
 // Ruta para generar el turno
 app.post('/generar-turno', (req, res) => {
     const { tipoTurno } = req.body;
     let letraTipoTurno;
+    let turno;
 
-    // Determinar la letra correspondiente al tipo de turno
+    // Determinar la letra correspondiente al tipo de turno y generar el turno adecuado
     switch (tipoTurno) {
         case 'credito':
             letraTipoTurno = 'C';
+            ultimoNumeroCredito++;
+            turno = `${letraTipoTurno}${ultimoNumeroCredito}`;
             break;
         case 'asesoria':
             letraTipoTurno = 'A';
+            ultimoNumeroAsesoria++;
+            turno = `${letraTipoTurno}${ultimoNumeroAsesoria}`;
             break;
         case 'caja':
             letraTipoTurno = 'K';
+            ultimoNumeroCaja++;
+            turno = `${letraTipoTurno}${ultimoNumeroCaja}`;
             break;
         default:
             return res.status(400).json({ error: 'Tipo de turno no válido' });
     }
-
-    // Generar un número de turno consecutivo
-    ultimoNumeroTurno++;
-    const turno = `${letraTipoTurno}${ultimoNumeroTurno}`;
 
     res.json({ turno });
 });
@@ -41,6 +54,7 @@ app.get('/ver-turno', (req, res) => {
     res.send(`<h1>Turno: ${turno}</h1><p>Tipo de turno: ${tipoTurno}</p>`);
 });
 
+// Inicializar el servidor
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
